6f6394b4841a641b8ab95cf3fad7f75086be4bec
Carlos Henrique <carlosbarros@unimedrj.coop.br>
2026-05-28 19:51:24 -0300

    draw a basic line

--- src/main.cpp
+++ src/main.cpp
@@ -2,58 +2,76 @@
 #include <iostream>
 #include <cstdlib>
 
+void drawBaseLine(SDL_Renderer* renderer) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+  SDL_RenderDrawLine(renderer, 100, 100, 300, 100);
+}
+
+void drawUsageLine(SDL_Renderer* renderer) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+  SDL_RenderDrawLine(renderer, 100, 100, 200, 100);
+}
+
 int main(int argc, char* argv[]) {
-  const int default_width = 800;
-  const int default_height = 600;
+    const int default_width = 800;
+    const int default_height = 600;
 
-  SDL_Window* window;
-
-  if (argc < 2) {
-    window = SDL_CreateWindow("Core++", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, default_width, default_height, SDL_WINDOW_SHOWN);
-  } else {
-    window = SDL_CreateWindow("Core++", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, std::atoi(argv[1]), std::atoi(argv[2]), SDL_WINDOW_SHOWN);
-  }
-
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
       std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        
       return 1;
-  }
+    }
 
-  if (!window) {
+    int width = default_width;
+    int height = default_height;
+
+    if (argc >= 3) {
+      width = std::atoi(argv[1]);
+      height = std::atoi(argv[2]);
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Core++", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+    if (!window) {
       std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+
       SDL_Quit();
       return 1;
-  }
+    }
 
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (!renderer) {
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
       std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+
       SDL_DestroyWindow(window);
       SDL_Quit();
       return 1;
-  }
-
-  bool running = true;
-  SDL_Event event;
-
-  while (running) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        running = false;
-      }
     }
 
-    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+    bool running = true;
+    SDL_Event event;
 
-    SDL_RenderClear(renderer);
+    while (running) {
+      while (SDL_PollEvent(&event)) {
+          if (event.type == SDL_QUIT) {
+              running = false;
+          }
+      }
 
-    SDL_RenderPresent(renderer);
-  }
+      SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+      SDL_RenderClear(renderer);
+      
+      drawBaseLine(renderer);
+      drawUsageLine(renderer);
 
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+      SDL_RenderPresent(renderer);
+    }
 
-  SDL_Quit();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 
-  return 0;
+    SDL_Quit();
+
+    return 0;
 }
